8f6e8cc11c1d1e76149ec5fc3042eabf74eaeffb
praweenanuwangee <praweenanuwangi@gmail.com>
2025-01-21 13:07:23 +0530

    upto connection

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 const express = require("express")
 const app = express()
 const dotenv = require("dotenv").config()
+
 const PORT= process.env.PORT || 3000
 
 
-app.get ("/", (req,res)=>{
-    res.json({message:"hello"})
-})
+app.use("/recipe",require("./routes/recipe"))
 
 
 app.listen(PORT,(err)=>{
