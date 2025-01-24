9b3a3979334927765b68dbfd27d8446fe51c4f0c
praweenanuwangee <praweenanuwangi@gmail.com>
2025-01-24 23:39:33 +0530

    change

--- frontend/Cook-Craft-App/src/components/RecipeItems.jsx
+++ frontend/Cook-Craft-App/src/components/RecipeItems.jsx
@@ -7,15 +7,7 @@ export default function RecipeItems() {
     <div>
       <h2>Recipe Items</h2>
       {/* Render the recipe items */}
-      <ul>
-        {allRecipes.length > 0 ? (
-          allRecipes.map((recipe, index) => (
-            <li key={index}>{recipe.name}</li> // Adjust this to match your recipe structure
-          ))
-        ) : (
-          <p>No recipes available</p>
-        )}
-      </ul>
+      
     </div>
   );
 }
