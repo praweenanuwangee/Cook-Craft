e0b0d4799817a7a15b934bbbc3fcee1df69060f1
praweenanuwangee <praweenanuwangi@gmail.com>
2025-01-25 23:24:14 +0530

    recipeItem

--- frontend/Cook-Craft-App/src/components/RecipeItems.jsx
+++ frontend/Cook-Craft-App/src/components/RecipeItems.jsx
@@ -1,13 +1,32 @@
 import React, { useState } from 'react'; // Import useState
-
+import foodImg from '../assets/download.jpeg'
+import { BsStopwatchFill } from "react-icons/bs";
+import { FaHeart } from "react-icons/fa6";
 export default function RecipeItems() {
   const [allRecipes, setAllRecipes] = useState([]);
 
   return (
-    <div>
-      <h2>Recipe Items</h2>
-      {/* Render the recipe items */}
-      
-    </div>
-  );
+    <>
+        <div className='card-container'>
+            {
+                allRecipes?.map((item, index) => {
+                    return (
+                        <div key={index} className='card'>
+                            <img src={foodImg} width="120px" height="100px"></img>
+                            <div className='card-body'>
+                                <div className='title'>{item.title}</div>
+                                <div className='icons'>
+                                    <div className='timer'><BsStopwatchFill/>30min</div>
+                                    <FaHeart/>
+                                       
+                        
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    </>
+);
 }
