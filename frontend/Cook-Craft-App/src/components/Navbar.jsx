2f0728a42d6e9b0e53738b2da40fc3ce1cdce8dd
praweenanuwangee <praweenanuwangi@gmail.com>
2025-01-31 13:16:24 +0530

    modal.jsx

--- frontend/Cook-Craft-App/src/components/Navbar.jsx
+++ frontend/Cook-Craft-App/src/components/Navbar.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import Modal from "./Modal";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const checkLogin = () => {
+    setIsOpen(true);
+  };
+
   return (
     <>
-        <header>
-            <h2>
-                Cook Craft
-            </h2>
-            <ul>
-                <li>Home</li>
-                <li>My Recipe</li>
-                <li>Favourites</li>
-                <li>Login</li>
-            </ul>
-        </header>
+      <header>
+        <h2>Cook Craft</h2>
+        <ul>
+          <li>Home</li>
+          <li>My Recipe</li>
+          <li>Favourites</li>
+          <li onClick={checkLogin}>Login</li>
+        </ul>
+      </header>
+      {isOpen && <Modal onClose={()=>setIsOpen(false)} />}
     </>
-  )
+  );
 }
